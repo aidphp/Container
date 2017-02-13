046737a81cde0939188941dd0901ee10231dc000
aidphp <maximebourmaut@gmail.com>
2017-02-13 19:11:24 +0100

    fix space

--- src/Container.php
+++ src/Container.php
@@ -6,48 +6,48 @@ use Interop\Container\ContainerInterface;
 
 class Container implements ContainerInterface
 {
-	protected $services = array();
-	protected $factories = array();
-	protected $shared = array();
+    protected $services = array();
+    protected $factories = array();
+    protected $shared = array();
 
-	public function register($id, callable $factory, $shared = true)
-	{
-		$this->factories[$id] = $factory;
-		$this->shared[$id] = (bool) $shared;
-		return $this;
-	}
+    public function register($id, callable $factory, $shared = true)
+    {
+        $this->factories[$id] = $factory;
+        $this->shared[$id] = (bool) $shared;
+        return $this;
+    }
 
-	public function set($id, $service)
-	{
-		$this->services[$id] = $service;
-		return $this;
-	}
+    public function set($id, $service)
+    {
+        $this->services[$id] = $service;
+        return $this;
+    }
 
-	public function get($id)
-	{
-		if (isset($this->services[$id]))
-		{
-			return $this->services[$id];
-		}
-		
-		if (!isset($this->factories[$id]))
-		{
-			throw new ServiceNotFoundException(sprintf('The service "%s" does not exist.', $id));
-		}
-		
-		$service = $this->factories[$id]($this);
-		
-		if ($this->shared[$id])
-		{
-			$this->services[$id] = $service;
-			unset($this->factories[$id]);
-		}
-		
-		return $service;
-	}
+    public function get($id)
+    {
+        if (isset($this->services[$id]))
+        {
+            return $this->services[$id];
+        }
 
-	public function has($id)
-	{
-		return isset($this->services[$id]) || isset($this->factories[$id]);
-	}
+        if (!isset($this->factories[$id]))
+        {
+            throw new ServiceNotFoundException(sprintf('The service "%s" does not exist.', $id));
+        }
+
+        $service = $this->factories[$id]($this);
+
+        if ($this->shared[$id])
+        {
+            $this->services[$id] = $service;
+            unset($this->factories[$id]);
+        }
+
+        return $service;
+    }
+
+    public function has($id)
+    {
+        return isset($this->services[$id]) || isset($this->factories[$id]);
+    }
 }
